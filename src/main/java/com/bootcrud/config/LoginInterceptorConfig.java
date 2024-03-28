7325a797a95bb5213ae8befabefc311f9a13a376
Zhaobz <you@example.com15852210776@139.com>
2024-03-28 18:45:39 +0800

    拦截器，登录功能，session

--- src/main/java/com/bootcrud/config/LoginInterceptorConfig.java
+++ src/main/java/com/bootcrud/config/LoginInterceptorConfig.java
@@ -1,9 +1,44 @@
 package com.bootcrud.config;
 
+import com.bootcrud.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 处理器拦截器的注册，哪些请求放行或拦截
  */
+@Configuration //加载当前的拦截器并进行注册
 public class LoginInterceptorConfig implements WebMvcConfigurer {
+
+
+    //配置白名单
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        /** 配置拦截器 */
+        HandlerInterceptor interceptor = new LoginInterceptor();
+        //添加白名单
+        List<String> patterns = new ArrayList<>();
+        patterns.add("/bootstrap3/**");
+        patterns.add("/css/**");
+        patterns.add("/images/**");
+        patterns.add("/js/**");
+        patterns.add("/web/register.html");
+        patterns.add("/web/login.html");
+        patterns.add("/web/index.html");
+        patterns.add("/user/reg");
+        patterns.add("/user/login");
+
+
+
+        registry.addInterceptor(interceptor)
+                .addPathPatterns("/**") //表示要拦截的url是什么  /**代表所有的请求都被拦截
+                .excludePathPatterns(patterns);  //白名单设置patterns
+
+    }
 }
--- src/main/java/com/bootcrud/mapper/UserMapper.java
+++ src/main/java/com/bootcrud/mapper/UserMapper.java
@@ -3,6 +3,9 @@ package com.bootcrud.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.bootcrud.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
 
 
 public interface UserMapper extends BaseMapper<User> {
@@ -19,4 +22,26 @@ public interface UserMapper extends BaseMapper<User> {
      * @return 如果找到返回这个用户的数据，没有找到返回null
      */
     User findByUsername(String username);
+
+    /**
+     * 据userId修改密码
+     * @param userId 用户的id
+     * @param userPassword 新密码
+     * @param modifiedUser 修改人
+     * @param modifiedTime  修改时间
+     * @return 如果成功返回受影响的行数
+     */
+    Integer updatePasswordByid(@Param("userId") Integer userId,
+                               @Param("userPassword") String userPassword,
+                               @Param("modifiedUser") String modifiedUser,
+                               @Param("modifiedTime") String modifiedTime);
+
+    /**
+     * 根据userId查询用户数据
+     * @param userId
+     * @return User/null
+     */
+    User findByUserId(Integer userId);
+
+
 }
